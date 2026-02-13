faa1d9f3ea36df4223f449fffbc5219d565bc592
Omolola Igbasan <lolabakre@gmail.com>
2026-02-13 13:00:55 -0600

    a

---
+++ week_2/task_1.py
@@ -0,0 +1,25 @@
+# # STUDENT GRADE INPUT With Grade class
+# #Ensure you are able to filder out wrong input
+# # 3.5 - 4.00 first class honours
+# # 3.0
+# #
+# #You are going to get input from a student. And then tell them their class of grade. Ensure that you are able to filter out wrong input
+
+# 3.5- 4.00 – First Class Honours
+# 3.0-3.49 – Second Class Honours (Upper Division)
+# 2.0-2.99 – Second Class Honours (Lower Division)
+# 1.0-1.99 – Third Class Honours
+
+grade = float (input ("Enter your grade"))
+if grade >= 3.5:
+    print("First Class Honours")
+#if grade >= 3 or :
+   # print("Invalid entry")
+elif grade >= 3.49:
+    print("Second Class Honours (Upper Division)")
+elif grade >= 2.99:
+    print("Second Class Honours (Lower Division)")
+else:
+    print("Third class honours")
+
+    # filter out wrong output
