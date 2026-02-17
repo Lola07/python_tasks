9dd9c1013abd4da68f45494d5c2bf3673d6a2650
Omolola Igbasan <lolabakre@gmail.com>
2026-02-16 20:47:30 -0600

    0

---
+++ week_2/task_2.py
@@ -0,0 +1,4 @@
+# Write a Python program that iterates through integers from 1 to 100. For each multiple of three, 
+# print "Fizz" instead of the number; 
+# for each multiple of five, print "Buzz". 
+# For numbers that are multiples of both three and five, print "FizzBuzz".
