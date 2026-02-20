474a852001f57b6a04ec4689efd07b4769abb22a
Omolola Igbasan <lolabakre@gmail.com>
2026-02-20 12:22:00 -0600

    w

---
+++ week_3/task_1.py
@@ -0,0 +1,11 @@
+
+
+   #make a classification category
+sales = [ 120, 450, 800, 50, 900, 300]
+for sale in sales:
+ if sale <= 300:
+    print("Low")
+ elif sale  <= 700:
+   print("Medium")
+ else:
+   print("High")
