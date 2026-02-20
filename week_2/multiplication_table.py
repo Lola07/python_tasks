531011e6625b2d55d214e910540af324ade5ba8a
Omolola Igbasan <lolabakre@gmail.com>
2026-02-20 08:14:35 -0600

    ok

---
+++ week_2/multiplication_table.py
@@ -0,0 +1,6 @@
+# create a multiplication table for 7 times table up to 10
+
+number = int(input("Enter a number : "))
+for numbers in range (1,11):
+    print(f"{number} x {numbers} = {number * numbers}")
+    
